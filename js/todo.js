aa7a45dc54e1508c3aec727c0467ba9f9ab80aca
José Henrique Ivanchechen <joseivanchechen@outlook.com>
2019-04-06 13:15:47 -0300

    finished functionality of the list

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,34 @@
-var todoList = [{id:0,title:"Learn something",desc:"Learn something",urgent:0},
-                {id:1,title:"Finish this",desc:"Finish this todo list",urgent:1},
-                {id:2,title:"Tune CSS",desc:"Tune CSS of this page",urgent:0}];
+var todoList = [{id:0,title:"Learn something",desc:"Learn something",urgent:false},
+                {id:1,title:"Finish this",desc:"Finish this todo list",urgent:true},
+                {id:2,title:"Tune CSS",desc:"Tune CSS of this page",urgent:false}];
+
+todoList = JSON.parse(localStorage.getItem("todo"));
 
 $(document).ready(function(){showTodo()});
 
 function showTodo(){
+    // Add urgent first
     for(var i = 0; i < todoList.length; i++){
+        if(!todoList[i].urgent) continue;
         var todoNode = document.createElement("li");
-        todoNode.appendChild(document.createTextNode(todoList[i].title));
         todoNode.id = "todo_" + i;
-        todoNode.setAttribute("onclick","clickTodo("+i+")")
+        var todoSpan = document.createElement("span");
+        todoSpan.appendChild(document.createTextNode(todoList[i].title));
+        todoSpan.setAttribute("onclick","clickTodo("+i+")")
+        todoSpan.title = todoList[i].desc;
+        todoNode.appendChild(todoSpan);
+        $('#todoList').append(todoNode);
+    }
+    // Add non-urgent
+    for(var i = 0; i < todoList.length; i++){
+        if(todoList[i].urgent) continue;
+        var todoNode = document.createElement("li");
+        todoNode.id = "todo_" + i;
+        var todoSpan = document.createElement("span");
+        todoSpan.appendChild(document.createTextNode(todoList[i].title));
+        todoSpan.setAttribute("onclick","clickTodo("+i+")")
+        todoSpan.title = todoList[i].desc;
+        todoNode.appendChild(todoSpan);
         $('#todoList').append(todoNode);
     }
 }
@@ -21,21 +40,32 @@ function refreshTodo(){
         i++;
     }
     showTodo();
+    localStorage.setItem("todo", JSON.stringify(todoList));
 }
 
 function clickTodo(id){
     $('#todo_'+id).css("text-decoration", "line-through");
     $('#todo_'+id).css("background-color", "gray");
+    $('#todo_'+id).css("font-size", "0em");
     for(var i = id+1; i < todoList.length; i++){
         todoList[i].id--;
-        console.log(todoList[i].id);
     }
     todoList.splice(id,1);
     setTimeout(function(){
         refreshTodo();
-    }, 2000);
+    }, 750);
 }
 
-function add(){
-    
+function addTask(event){
+    var id = getNextTaskID();
+    var name = $("#taskTitle").val();
+    var desc = $("#taskDesc").val();
+    var urg = $("#taskUrgent").is(":checked");
+    console.log(name);
+    todoList.push({id:id,title:name,desc:desc,urgent:urg});
+    refreshTodo();
+}
+
+function getNextTaskID(){
+    return todoList.length;
 }
